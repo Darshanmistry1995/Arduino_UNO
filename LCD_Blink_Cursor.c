92b223de786952808843cc83c335d5d818ee6856
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-05-01 23:48:26 -0400

    Update LCD_Blink_Cursor.c

--- LCD_Blink_Cursor.c
+++ LCD_Blink_Cursor.c
@@ -1,12 +1,12 @@
 //Darshan Mistry...
 #include <LiquidCrystal_I2C.h> //init header file for I2C Interfacing..
 
-LiquidCrystal_I2C lcd(0x27, 20, 4); //port connection init.
+LiquidCrystal_I2C lcd(0x27, 20, 4); //port connection init...
 void setup() 
 {
   lcd.init();
   lcd.backlight();
-  lcd.print("Darshan Mistry.!"); //print the character
+  lcd.print("Darshan Mistry.!"); //print the character...
 }
 //Loop..
 void loop()
